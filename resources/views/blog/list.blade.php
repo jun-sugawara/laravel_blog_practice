597bc4189029ddd6a295fa55f493e2bfb32fb1af
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-12-11 15:34:44 +0900

    viewの切り分け

--- resources/views/blog/list.blade.php
+++ resources/views/blog/list.blade.php
@@ -1,6 +1,8 @@
-    <div class="row">
+@extends('layout')
+@section('content')
+<div class="row">
   <div class="col-md-8 col-md-offset-2">
-      <h2>ブログ記事一覧</h2>
+    <h2>ブログ記事一覧</h2>
       <table class="table table-striped">
           <tr>
               <th>記事番号</th>
@@ -15,6 +17,7 @@
               <td></td>
           </tr>
       </table>
+    </div>
   </div>
 </div>
-    </div>
+@endsection
