f06ee21a9cfe9dc1d88d24d475dede9f771c0173
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-12-12 11:28:57 +0900

    トップページにダミーデータを表示する処理の実装

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Blog;
 
 class BlogController extends Controller
 {
@@ -10,6 +11,9 @@ class BlogController extends Controller
     // @return view
     public function showList()
     {
-        return view('blog.list');
+        $blogs = Blog::all();
+
+        return view('blog.list',
+        ['blogs'=> $blogs]);
     }
 }
