bc58639beda1dcba3791edfb58fab425a6bf538c
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-12-12 10:04:00 +0900

    BlogsTableSeederファイルとBlogFactoryファイルの生成

---
+++ database/factories/BlogFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Blog;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class BlogFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    protected $model = Blog::class;
+    
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->word,
+            'content' => $this->faker->realText
+        ];
+    }
+}
