151fca9017c273f77924593aa90aad6079efb0ca
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-12-14 07:55:06 +0900

    route設定（ブログ登録画面表示・ブログ登録実行）

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,19 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+// 命名規則
+// show->表示する
+// exe->実行する
 
+// ブログ一覧画面を表示
 Route::get('/', 'App\Http\Controllers\BlogController@showList')->name('blogs');
+
+// ブログ登録画面を表示
+Route::get('/blog/create', 'App\Http\Controllers\BlogController@showCreate')->name('create');
+
+// ブログ登録
+Route::post('/blog/store', 'App\Http\Controllers\BlogController@exeStore')->name('store');
+
+// ブログ詳細画面を表示
 Route::get('/blog/{id}', 'App\Http\Controllers\BlogController@showDetail')->name('show');
+
