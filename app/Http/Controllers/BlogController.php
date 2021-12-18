92b8208f92b40e8c0b54e684b096e2650d87c7cc
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-12-18 11:52:29 +0900

    ブログ更新処理を実装

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -76,4 +76,30 @@ class BlogController extends Controller
         return view('blog.edit',
         ['blog'=> $blog]);
     }
+
+    // ブログを登録する
+    // @return view    
+    public function exeUpdate(BlogRequest $request)
+    {
+        // ブログのデータを受け取る
+        $inputs = $request->all();
+        
+        \DB::beginTransaction();
+        try{
+            // ブログを更新する
+           $blog = Blog::find($inputs['id']);
+           $blog->fill([
+            'title' => $inputs['title'],
+            'content' => $inputs['content']
+           ]);
+           $blog->save();
+            \DB::commit();
+        } catch(\Throwable $e){
+            \DB::rollback();
+            abort(500);
+        }
+
+        \Session::flash('err_msg', 'ブログを更新しました。');
+        return redirect(route('blogs'));
+    }
 }
